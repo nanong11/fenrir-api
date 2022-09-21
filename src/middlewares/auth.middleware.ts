de4ac5faf93b6c499d9acefa7abb9e2aa0e529e3
nanong11 <marcallen.nanong11@gmail.com>
2022-09-21 19:22:07 +0800

    Disbale use of cookie

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -7,8 +7,10 @@ import userModel from '@models/users.model';
 
 const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
   try {
-    const Authorization = req.cookies['Authorization'] || (req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null);
-
+    // Disable use of cookie but still set in
+    // const Authorization = req.cookies['Authorization'] || (req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null);
+    const Authorization = req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null;
+    // console.log('Authorization', Authorization);
     if (Authorization) {
       const secretKey: string = SECRET_KEY;
       const verificationResponse = (await verify(Authorization, secretKey)) as DataStoredInToken;
