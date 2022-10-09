e6c3dd84fb868c1f9a232f20946584265246820e
nanong11 <marcallen.nanong11@gmail.com>
2022-10-10 00:22:11 +0800

    Add upload limit file size

--- src/app.ts
+++ src/app.ts
@@ -60,8 +60,8 @@ class App {
     this.app.use(hpp());
     this.app.use(helmet());
     this.app.use(compression());
-    this.app.use(express.json({ limit: '10mb' }));
-    this.app.use(express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }));
+    this.app.use(express.json({ limit: '100mb' }));
+    this.app.use(express.urlencoded({ limit: '100mb', extended: true, parameterLimit: 50000 }));
     this.app.use(cookieParser());
   }
 
