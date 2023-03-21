c602152e04f68205919e4b27059bbeaaad60b749
nanong11 <marcallen.nanong11@gmail.com>
2023-03-21 22:40:32 +0800

    Ongoing development in andvari chat

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -39,12 +39,12 @@ class MessageService {
   public async createMessage(messageData: CreateMessageDto): Promise<Message> {
     if (isEmpty(messageData)) throw new HttpException(400, 'messageData is empty');
 
-    const findConversationById: Conversation = await this.conversationService.findConversationById(messageData.conversationId);
-    if (!findConversationById) throw new HttpException(409, "conversation doesn't exist");
+    // const findConversationById: Conversation = await this.conversationService.findConversationById(messageData.conversationId);
+    // if (!findConversationById) throw new HttpException(409, "conversation doesn't exist");
 
-    const participants: Array<any> = findConversationById.participants;
-    const isExist = participants.find(participant => participant.userId === messageData.userId);
-    if (isEmpty(isExist)) throw new HttpException(400, 'user is not a participant of the conversation');
+    // const participants: Array<any> = findConversationById.participants;
+    // const isExist = participants.find(participant => participant.userId === messageData.userId);
+    // if (isEmpty(isExist)) throw new HttpException(400, 'user is not a participant of the conversation');
 
     const createMessage: Message = await this.message.create({ ...messageData });
     if (isEmpty(createMessage)) throw new HttpException(400, 'create message failed');
